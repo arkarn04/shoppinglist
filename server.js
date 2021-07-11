03e58b6ff772993a3a55ff53a8724b99cd4d02d2
arkarn04 <karnaryan253@gmail.com>
2021-07-11 19:57:25 +0530

    Version 1.0.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 5000
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -21,6 +22,16 @@ mongoose
 app.use(bodyParser.json());
 
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV == 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`);
